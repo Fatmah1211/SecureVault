7c73735b337e9e4182f091497c2b18f0a47f43a8
maidasaleem1113 <maidasaleem1113@gmail.com>
2026-06-09 23:00:43 +0500

    feat: initialize generator-screen branch with skeleton class

---
+++ generator_screen.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+import random
+import string
+
+class GeneratorScreen(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        # UI will be built here
+        pass
